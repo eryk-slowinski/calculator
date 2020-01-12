d821003c5722d935f0ec6e74a1f3e7a90ec3cff6
Eryk Slowinski <e.slowinski92@gmail.com>
2020-01-12 22:41:50 +0100

    Refactoring

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ keyboard.addEventListener('click', function (event) {
         const arr = Array.from(event.target.parentNode.children);
         arr.forEach(button => button.classList.remove('highlighted'));
 
-        const action = event.target.dataset.action;
         const buttonValue = event.target.textContent;
         const displayedNumber = display.textContent;
+        const action = event.target.dataset.action;
 
         if (!action) {
             if (displayedNumber === '0' || keyboard.dataset.recentKeyType === 'operator' || keyboard.dataset.recentKeyType === 'calculate') {
@@ -37,8 +37,8 @@ keyboard.addEventListener('click', function (event) {
             action === 'subtract' ||
             action === 'divide' ||
             action === 'multiply') {
-            const firstArgument = parseFloat(keyboard.dataset.firstArgument);
-            const secondArgument = parseFloat(displayedNumber);
+            const firstArgument = keyboard.dataset.firstArgument;
+            const secondArgument = displayedNumber;
             const operator = keyboard.dataset.operator;
             if (firstArgument && operator && keyboard.dataset.recentKeyType !== 'operator' && keyboard.dataset.recentKeyType !== 'calculate') {
                 const calculatedValue = calculate(firstArgument, secondArgument, operator);
@@ -54,8 +54,8 @@ keyboard.addEventListener('click', function (event) {
         }
 
         if (action === 'calculate') {
-            let firstArgument = parseFloat(keyboard.dataset.firstArgument);
-            let secondArgument = parseFloat(displayedNumber);
+            let firstArgument = keyboard.dataset.firstArgument;
+            let secondArgument = displayedNumber;
             const operator = keyboard.dataset.operator;
             if (firstArgument) {
                 if (keyboard.dataset.recentKeyType === 'calculate') {
@@ -71,15 +71,10 @@ keyboard.addEventListener('click', function (event) {
 })
 
 const calculate = (a, b, operation) => {
-    let result;
-    if (operation === 'add') {
-        result = a + b;
-    } else if (operation === 'subtract') {
-        result = a - b;
-    } else if (operation === 'multiply') {
-        result = a * b;
-    } else if (operation === 'divide') {
-        result = a / b;
-    }
-    return result;
+    const x = parseFloat(a);
+    const y = parseFloat(b);
+    if (operation === 'add') return x + y;
+    if (operation === 'subtract') return x - y;
+    if (operation === 'multiply') return x * y;
+    if (operation === 'divide') return x / y;
 }
